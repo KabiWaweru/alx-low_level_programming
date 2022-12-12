7a687b813faa21b700ba37b0fe8d9172e5fc431b
Nyokabi Waweru <nyokabii.waweru@gmail.com>
2022-12-12 13:04:21 +0300

    Corrections made

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
 /**
  * main - This is the entry point of the program and puts() displays a string
  *
- * Return - returns zero to show success of program
+ * Return: returns zero to show success of program
  */
 
 int main(void)
