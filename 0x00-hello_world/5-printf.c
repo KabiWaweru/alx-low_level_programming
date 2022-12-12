5e3f09efe2dfebfc5d4f16cc3706215e4f731646
Nyokabi Waweru <nyokabii.waweru@gmail.com>
2022-12-12 12:58:40 +0300

    Corrections made

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,11 +3,11 @@
 /**
  *main - entry point, prints out string using printf
  *
- *Return - 0 if successful, 1 if error
+ *return - 0 if successful, 1 if error
  */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art, \n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
