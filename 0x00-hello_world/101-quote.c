f88162b7a2cd5f86f753749a82b5751be0ca3c01
Nyokabi Waweru <nyokabii.waweru@gmail.com>
2022-12-12 23:21:58 +0300

    corrections

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,7 @@
 
 int main(void)
 {
-	warnx("%s","and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	char string[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	write(2,string,sizeof(string));
 	return (0);
 }
