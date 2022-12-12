0b889a8f0f196c16859678689dd1a9f4c395cce7
Nyokabi Waweru <nyokabii.waweru@gmail.com>
2022-12-12 13:49:59 +0300

    advanced tasks

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this function prints to the standard error stream
+ *
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	warn("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (0);
+}
