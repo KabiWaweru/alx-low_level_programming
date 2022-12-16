b10396857d41ab6f3b8fb359b863ee7da2d2c39c
Nyokabi Waweru <nyokabii.waweru@gmail.com>
2022-12-16 03:30:48 +0300

    corrections

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,20 +15,19 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	char n_string [12];
-	sprintf(n_string,"%d",n);
-	char m = n_string[strlen(n_string)];
-	if (m > '5')
+	int m = n % 10;
+
+	if (m > 5)
 	{
 		printf("The last digit of n is %d and greater than 5\n", m);
 	}
 
-	else if (m > '0')
+	else if (m > 0)
         {
                 printf("The last digit of n is %d and greater than 5\n", m);
         }
 
-        else if ((m < '6') && (m != '0'))
+        else if ((m < 6) && (m != 0))
         {
                 printf("The last digit of n is %d and is less than 6 an not zero\n", m);
         }
