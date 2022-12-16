a3a6eda952bc32f588edf042b765d7a66d1d1d1a
Nyokabi Waweru <nyokabii.waweru@gmail.com>
2022-12-16 07:18:41 +0300

    added advanced tak

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations of triple digit numbers
+ *
+ * Return: 0, if successful
+ */
+
+int main(void)
+{
+	int n;
+	int m;
+	int p;
+
+	for (n = '0'; n < '8'; n++)
+	{
+		for (m = n + 1; m < '9'; m++)
+		{
+			for (p = m + 1; p <= '9'; p++)
+			{
+				putchar(n);
+				putchar(m);
+				putchar(p);
+				if (n == '8')
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
