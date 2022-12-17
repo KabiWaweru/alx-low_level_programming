e357ace4a2c9c42a0aae192e2ab7b9f3eb9dac48
Nyokabi Waweru <nyokabii.waweru@gmail.com>
2022-12-18 01:27:09 +0300

    added advanced task

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * main - This function prints all possible combinations of double numbers
+ *
+ * Return: 0, if successful
+ */
+
+int main(void)
+{
+	int m;
+	int p;
+	int q;
+	int r;
+
+	for (m = '0'; m <= '9'; m++)
+	{
+		for (p = '0'; p < '9'; p++)
+		{
+			for (q = '0'; q <= '9'; q++)
+			{
+				for (r = '0'; r <= '9'; r++)
+				{
+					putchar(m);
+					putchar(p);
+					putchar(' ');
+					putchar(q);
+					putchar(r);
+					if ((r == 99) && (q == 99) && (r == 99))
+					{
+						break;
+					}
+					putchar(',');
+					putchar(' ');
+				}
+
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
