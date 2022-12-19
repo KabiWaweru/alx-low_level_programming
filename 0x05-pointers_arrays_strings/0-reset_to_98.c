2731d9e79a88a17ab1e8123ee65a61341f7f0bef
Nyokabi Waweru <nyokabii.waweru@gmail.com>
2022-12-19 15:41:18 +0300

    added task

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - This function resets the value being pointed to 98
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,5 @@
+#ifndef _POINTERS_H
+#define _POINTERS_H
+
+void reset_to_98(int *n);
+#endif
